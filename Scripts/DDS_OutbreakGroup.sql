13f88c06705a57d36c2a72b9917b0401cf040d8b
Yen Nhi Le <leyennhi112@gmail.com>
2022-12-07 21:11:42 +0700

    Update DDS_OutbreakGroup.sql

--- Scripts/DDS_OutbreakGroup.sql
+++ Scripts/DDS_OutbreakGroup.sql
@@ -29,7 +29,6 @@ create table Fact_Outbreak(
     number_of_outbreak bigint not null
 )
 
-Dim_OutbreakGroup
 alter table Fact_Outbreak add 
     constraint FK_Fact_Outbreak_Dim_Date    		foreign key (date_id)        		references Dim_Date (date_id),
     constraint FK_Fact_Outbreak_Dim_Geography   	foreign key (geography_id)      	references Dim_Geography (geography_id),
